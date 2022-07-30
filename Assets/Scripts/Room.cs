1142fabecf6e3209aa5dbc438c29626896ffbf70
Mikhail Uchunzhyan <36170426+muchunzyan@users.noreply.github.com>
2022-07-30 12:05:33 +0300

    midgard tileset added

--- Assets/Scripts/Room.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Room : MonoBehaviour
-{
-    public string roomName;
-    public List<string> possibleRooms = new List<string>();
-}
